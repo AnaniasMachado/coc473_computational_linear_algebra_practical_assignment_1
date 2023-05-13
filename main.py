d3217733772ff0680268574f6345208900726ac5
Ananias <86498679+ananiasmachado@users.noreply.github.com>
2023-05-13 16:37:51 -0300

    Adding a forgotten comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Purpose: 
+Purpose: Verify the results of the implemented algorithms for a set of matrix and vectors.
 """
 
 """ Libraries """
@@ -21,51 +21,49 @@ if __name__ == "__main__":
     vectors = ["./data/Vetor_B_01.dat", "./data/Vetor_B_02.dat", "./data/Vetor_B_03.dat"]
 
     # Solving a linear system
-    """
-    A = read_matrix_file(matrix)
-    B = make_matrix(vectors)
     
-    method = "LU"
-    X = linear_system_solver(A, B, method)
-    print(X)
-
-    A = read_matrix_file(matrix)
-    B = make_matrix(vectors)
-
-    method = "Cholesky"
-    X = linear_system_solver(A, B, method)
-    print(X)
-
-    A = read_matrix_file(matrix)
-    B = make_matrix(vectors)
+    # A = read_matrix_file(matrix)
+    # B = make_matrix(vectors)
     
-    method = "Jacobi"
-    X = linear_system_solver(A, B, method)
-    print(X)
+    # method = "LU"
+    # X = linear_system_solver(A, B, method)
+    # print(X)
 
-    A = read_matrix_file(matrix)
-    B = make_matrix(vectors)
+    # A = read_matrix_file(matrix)
+    # B = make_matrix(vectors)
+
+    # method = "Cholesky"
+    # X = linear_system_solver(A, B, method)
+    # print(X)
+
+    # A = read_matrix_file(matrix)
+    # B = make_matrix(vectors)
     
-    method = "Gauss-Seidel"
-    X = linear_system_solver(A, B, method)
-    print(X)
-    """
+    # method = "Jacobi"
+    # X = linear_system_solver(A, B, method)
+    # print(X)
+
+    # A = read_matrix_file(matrix)
+    # B = make_matrix(vectors)
+    
+    # method = "Gauss-Seidel"
+    # X = linear_system_solver(A, B, method)
+    # print(X)
 
     # Computing eigenvectors and eigenvalues
-    # """
-    M = read_matrix_file(matrix)
-    
-    method = "Power Method"
-    eigenvectors, eigenvalues, determinant = eigenvector_eigenvalue_solver(M, method, determinant=True)
-    print(eigenvectors)
-    print(eigenvalues)
-    print(determinant)
 
-    M = read_matrix_file(matrix)
+    # M = read_matrix_file(matrix)
     
-    method = "Jacobi Eigenvalue Algorithm"
-    eigenvectors, eigenvalues, determinant = eigenvector_eigenvalue_solver(M, method, determinant=True)
-    print_matrix(eigenvectors)
-    print_matrix(eigenvalues)
-    print(determinant)
-    # """
+    # method = "Power Method"
+    # eigenvectors, eigenvalues, determinant = eigenvector_eigenvalue_solver(M, method, determinant=True)
+    # print(eigenvectors)
+    # print(eigenvalues)
+    # print(determinant)
+
+    # M = read_matrix_file(matrix)
+    
+    # method = "Jacobi Eigenvalue Algorithm"
+    # eigenvectors, eigenvalues, determinant = eigenvector_eigenvalue_solver(M, method, determinant=True)
+    # print_matrix(eigenvectors)
+    # print_matrix(eigenvalues)
+    # print(determinant)
